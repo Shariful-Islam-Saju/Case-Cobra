6e74297a00d218130c6f49537c4ad1956831e528
Shariful Islam <sajukhan12905@gmail.com>
2024-11-27 20:25:12 +0600

    going to copy paste design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,8 +147,12 @@ export default function Home() {
                 />
                 <div className="flex flex-col">
                   <p className="font-semibold">
-                    
+                    Akash Sky
                   </p>
+                  <div className=" flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
