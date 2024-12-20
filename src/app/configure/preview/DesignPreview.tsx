0971c0435170c55c185bd37c1258365525b003a8
Shariful Islam <sajukhan12905@gmail.com>
2024-12-20 20:25:36 +0600

    building preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Phone from "@/components/Phone";
-import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/product";
+import { cn, formatPrice } from "@/lib/utils";
 import { COLORS, MODEL } from "@/validators/option-validator";
 import { Configuration } from "@prisma/client";
+import { ArrowRight, Check } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
 
@@ -10,8 +13,8 @@ interface pageProps {
   configuration: Configuration;
 }
 const DesignPreview = ({ configuration }: pageProps) => {
+  const { color, model, finish, material } = configuration;
   const [showConfetti, setShowConfetti] = useState(false);
-  const { color, model } = configuration;
   const tw = COLORS.find(
     (supportedColor) => supportedColor.value === color
   )?.tw;
@@ -20,6 +23,12 @@ const DesignPreview = ({ configuration }: pageProps) => {
   const { label: modelLabel } = MODEL.options.find(
     ({ value }) => value === model
   )!;
+
+  const totalPrice = formatPrice(
+    BASE_PRICE +
+      (finish === "textured" ? PRODUCT_PRICES.finish.textured : 0) +
+      (material === "polycarbonate" ? PRODUCT_PRICES.material.polycarbonate : 0)
+  );
 
   useEffect(() => setShowConfetti(true), []);
   return (
@@ -39,6 +48,79 @@ const DesignPreview = ({ configuration }: pageProps) => {
             className={cn(`bg-${tw}`)}
             imgSrc={configuration.croppedImageUrl!}
           />
+        </div>
+        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case{" "}
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorption</li>
+                <li>Packaging made from recycled materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High-quality, durable material</li>
+                <li>Scratch- and fingerprint resistant coating</li>
+              </ol>
+            </div>
+          </div>
+
+          <div className="mt-8">
+            <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+              <div className="flow-root text-sm">
+                <div className="flex items-center justify-between py-1 mt-2">
+                  <p className="text-gray-600">Base price</p>
+                  <p className="font-medium text-gray-900">
+                    {formatPrice(BASE_PRICE / 100)}
+                  </p>
+                </div>
+
+                {finish === "textured" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Textured finish</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.finish.textured)}
+                    </p>
+                  </div>
+                ) : null}
+
+                {material === "polycarbonate" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Soft polycarbonate material</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.material.polycarbonate)}
+                    </p>
+                  </div>
+                ) : null}
+
+                <div className="my-2 h-px bg-gray-200" />
+
+                <div className="flex items-center justify-between py-2">
+                  <p className="font-semibold text-gray-900">Order total</p>
+                  <p className="font-semibold text-gray-900">{totalPrice}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-8 flex justify-end pb-12">
+              <Button className="px-4 sm:px-6 lg:px-8 ">
+                Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </>
