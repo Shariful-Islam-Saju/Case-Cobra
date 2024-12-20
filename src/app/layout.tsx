abdea92960431d2864c4641b0c57e249bb036b8f
Shariful Islam <sajukhan12905@gmail.com>
2024-12-20 20:38:47 +0600

    change some design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Provider from "@/components/Provider";
+import { Recursive } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const recursive = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Case-Cobra",
@@ -33,9 +24,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
 
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={recursive.className}>
         <Navbar />
 
         <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
