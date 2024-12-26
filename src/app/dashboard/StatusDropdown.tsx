45c9a9456e38e6163198f159c91ccec60d9b2698
Shariful Islam <sajukhan12905@gmail.com>
2024-12-26 23:39:02 +0600

    using dropdown

---
+++ src/app/dashboard/StatusDropdown.tsx
@@ -0,0 +1,19 @@
+import { OrderStatus } from "@prisma/client";
+import React from "react";
+
+const StatusDropdown = ({
+  id,
+  orderStatus,
+}: {
+  id: string;
+  orderStatus: OrderStatus;
+}) => {
+  const LABEL_MAP: Record<keyof typeof OrderStatus, string> = {
+    awaiting_shipment: "Awaiting Shipment",
+    fulfilled: "Fulfilled",
+    shipped: "Shipped",
+  };
+  return <div>StatusDropdown</div>;
+};
+
+export default StatusDropdown;
