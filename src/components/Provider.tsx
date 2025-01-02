4b2c79d61f8b69ededda9c744914815f00de6c16
Shariful Islam <sajukhan12905@gmail.com>
2025-01-02 11:34:02 +0600

    add kindeProvider

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,10 +1,25 @@
 "use client";
 import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
 const client = new QueryClient();
 const Provider = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+
+  return (
+    <KindeProvider
+      domain="your-kinde-domain"
+      clientId={process.env.KINDE_CLIENT_ID}
+      redirectUri={process.env.KINDE_POST_LOGIN_REDIRECT_URL}
+      logoutUri={process.env.KINDE_POST_LOGOUT_REDIRECT_URL}
+      cookieOptions={{
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Lax",
+      }}
+    >
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </KindeProvider>
+  );
 };
 
 export default Provider;
